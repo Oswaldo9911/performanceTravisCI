fe6f63c4ced4cd6b0159b241396ae5c0f3964ea8
Oswaldo9911 <oswaldobuitrago9911@gmail.com>
2020-09-15 20:36:08 -0500

    Report Validate 7

--- bin/generate_files_from_jtl.sh
+++ bin/generate_files_from_jtl.sh
@@ -3,4 +3,5 @@
 #cd /Users/oswaldobetancourt/repos/jmeter-bootstrap/apache-jmeter-5.1.1/apache-jmeter-5.1/bin
 #jmeter -g results/calculator.jtl -o results/JMeterReport/Generate
 
-cd /home/travis/build/Oswaldo9911/performanceTravisCI/apache-jmeter-5.1/bin/jmeter -g results/calculator.jtl -o results/JMeterReport/Generate
+cd /home/travis/build/Oswaldo9911/performanceTravisCI/apache-jmeter-5.1/bin
+jmeter -g results/calculator.jtl -o results/JMeterReport/Generate
