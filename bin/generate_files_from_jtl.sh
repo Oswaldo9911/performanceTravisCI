f6d78b14f28029d93113549d10cc1912fad5a2fb
Oswaldo9911 <oswaldobuitrago9911@gmail.com>
2020-10-27 17:26:54 -0500

    Update YML

--- bin/generate_files_from_jtl.sh
+++
@@ -1,7 +0,0 @@
-# Report
-
-#cd /Users/oswaldobetancourt/repos/jmeter-bootstrap/apache-jmeter-5.1.1/apache-jmeter-5.1/bin
-#jmeter -g results/calculator.jtl -o results/JMeterReport/Generate
-
-cd /home/travis/build/Oswaldo9911/performanceTravisCI/apache-jmeter-5.1/bin
-jmeter -g results/calculator.jtl -o results/JMeterReport/Generate
